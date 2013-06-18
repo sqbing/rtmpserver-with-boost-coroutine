95771d9d720b5574f321d2fcf7e1f44811ac3ba5
Been Woo <sqbing@gmail.com>
2013-06-18 16:24:47 +0800

    Init test branch for boost asio.

---
+++ src/client.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main(int argc, const char *argv[])
+{
+    std::cout<<"hello world"<<std::endl;
+    return 0;
+}
--- src/server.cpp
+++ src/server.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include <time.h>
+
+std::string make_daytime_string()
+{
+    time_t now = time(NULL);
+    return ctime(&now);
+}
 
 int main(int argc, const char *argv[])
 {
-    std::cout<<"hello world"<<std::endl;
+    try
+    {
+        boost::asio::io_service ios;
+        boost::asio::ip::tcp::acceptor acceptor(ios, 
+                boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 
+                                               65530));
+        for(int i=0; i<2; i++)
+        {
+            boost::asio::ip::tcp::socket socket(ios);
+            acceptor.accept(socket);
+            std::string daytime_string = make_daytime_string();
+
+            boost::system::error_code ignored_error;
+            boost::asio::write(socket, 
+                               boost::asio::buffer(daytime_string),
+                               ignored_error);
+        }
+    }catch(std::exception &e)
+    {
+        std::cout<<e.what()<<std::endl;
+    }
     return 0;
 }
